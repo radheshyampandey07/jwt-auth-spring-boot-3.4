e108da1b73e3c82ce9c3a7c171214b27ded53506
radheshyam <radheshyam@DESKTOP-0529BSA>
2025-05-20 23:25:52 +0530

    checking app

--- jwt-demo/src/main/java/com/jwt/demo/user/UserController.java
+++ jwt-demo/src/main/java/com/jwt/demo/user/UserController.java
@@ -1,5 +1,10 @@
 package com.jwt.demo.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
 }

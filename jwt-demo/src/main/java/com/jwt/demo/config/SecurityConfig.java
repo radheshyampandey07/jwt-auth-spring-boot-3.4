2184d2320945fba5ea5eb8743d67033c1bd35cc1
radhepandey07 <radhepandey05@gmail.com>
2025-06-18 17:22:27 +0530

    User details service added

--- jwt-demo/src/main/java/com/jwt/demo/config/SecurityConfig.java
+++ jwt-demo/src/main/java/com/jwt/demo/config/SecurityConfig.java
@@ -1,15 +1,23 @@
 package com.jwt.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
@@ -20,8 +28,16 @@ public class SecurityConfig {
 				// enable form login
 				.formLogin(Customizer.withDefaults())
 				// for other rest requests
-				.httpBasic(Customizer.withDefaults());
+				.httpBasic(Customizer.withDefaults())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
 		return security.build();
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		return provider;
 	}
 
 }
